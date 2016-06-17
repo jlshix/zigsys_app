727ca855769c4fff1835c53d25feb370f257fbb7
jlshix <jlshix@163.com>
2016-06-17 22:47:44 +0800

    为了推送很心酸

--- app/src/main/java/com/jlshix/zigsys/App.java
+++ app/src/main/java/com/jlshix/zigsys/App.java
@@ -4,6 +4,11 @@ import android.app.Application;
 
 import org.xutils.x;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import cn.jpush.android.api.JPushInterface;
+
 /**
  * Created by Leo on 2016/6/14.
  * App类
@@ -12,7 +17,17 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        // xUtils
         x.Ext.init(this);
         x.Ext.setDebug(false);
+
+        //JPush
+        JPushInterface.setDebugMode(false);
+        JPushInterface.init(this);
+        Set<String> tags = new HashSet<>();
+        tags.add("jlshix");
+        JPushInterface.setTags(getApplicationContext(), tags, null);
+
     }
 }
