fc93c2eaf37adc413d7be8f70b8c8125486a1e3a
jlshix <jlshix@163.com>
2016-06-18 23:01:32 +0800

    网关绑定可用

--- app/src/main/java/com/jlshix/zigsys/GateBindActivity.java
+++ app/src/main/java/com/jlshix/zigsys/GateBindActivity.java
@@ -1,9 +1,7 @@
 package com.jlshix.zigsys;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
-import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
 import org.xutils.view.annotation.ContentView;
