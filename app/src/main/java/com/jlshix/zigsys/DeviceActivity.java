7382f1fe120755fc27f1ec6d94f4e303c7cd6965
jlshix <jlshix@163.com>
2016-06-19 18:59:40 +0800

    去除fab 通过menu添加设备

--- app/src/main/java/com/jlshix/zigsys/DeviceActivity.java
+++ app/src/main/java/com/jlshix/zigsys/DeviceActivity.java
@@ -121,7 +121,7 @@ public class DeviceActivity extends BaseActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_add) {
             return true;
         }
 
