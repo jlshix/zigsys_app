2bb177cf6c49379f1fab1bf80b67c34072338d3b
jlshix <jlshix@163.com>
2016-06-19 18:54:59 +0800

    引入SharedPreference

--- app/src/main/java/com/jlshix/zigsys/App.java
+++ app/src/main/java/com/jlshix/zigsys/App.java
@@ -1,6 +1,7 @@
 package com.jlshix.zigsys;
 
 import android.app.Application;
+import android.content.SharedPreferences;
 
 import org.xutils.x;
 
@@ -14,6 +15,9 @@ import cn.jpush.android.api.JPushInterface;
  * App类
  */
 public class App extends Application {
+
+    public static SharedPreferences sp;
+
     @Override
     public void onCreate() {
         super.onCreate();
@@ -28,6 +32,8 @@ public class App extends Application {
         Set<String> tags = new HashSet<>();
         tags.add("jlshix");
         JPushInterface.setTags(getApplicationContext(), tags, null);
+
+        sp = getSharedPreferences("conf", MODE_PRIVATE);
 
     }
 }
